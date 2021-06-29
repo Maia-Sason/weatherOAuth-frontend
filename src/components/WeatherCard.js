e2ada8c12e7939c2a20fe051da1e81f4c1288c8c
Maia Sason <mayasasson2000@gmail.com>
2021-06-29 09:20:13 -0400

    Added icons

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,7 +1,8 @@
-import { Paper, Box, Typography } from "@material-ui/core";
+import { Paper, Box, Typography, SvgIcon } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 import { blue, green, orange, red, yellow } from "@material-ui/core/colors";
 import { PaperBG } from "./PaperBG";
+import { ReactComponent as WeatherIcon } from "../icons/blizzard.svg";
 
 const WeatherCard = ({ city, icon, farenheight, feelsLike, description }) => {
   const image = PaperBG(icon);
@@ -20,8 +21,6 @@ const WeatherCard = ({ city, icon, farenheight, feelsLike, description }) => {
       zIndex: 1,
       backgroundColor: "transparent",
       color: "white",
-      textAlign: "right",
-
       "&::before": {
         content: `""`,
         filter: "blur(3px) brightness(.95)",
@@ -36,6 +35,15 @@ const WeatherCard = ({ city, icon, farenheight, feelsLike, description }) => {
         width: "105%",
       },
     },
+    contentContainer: {
+      textAlign: "left",
+      padding: "2em",
+    },
+    contentMain: {
+      display: "flex",
+      justifyContent: "space-between",
+      paddingBottom: "2em",
+    },
     textContainer: {
       margin: theme.spacing(0),
       position: "relative",
@@ -45,6 +53,17 @@ const WeatherCard = ({ city, icon, farenheight, feelsLike, description }) => {
     yellowPaper: {
       backgroundColor: yellow[300],
     },
+    weatherIcon: {
+      fill: "white",
+      opacity: 1,
+      height: "80px",
+      width: "auto",
+    },
+    cityName: {
+      position: "relative",
+      bottom: 0,
+      fontSize: "3em",
+    },
   }));
 
   const classes = useStyles();
@@ -52,13 +71,20 @@ const WeatherCard = ({ city, icon, farenheight, feelsLike, description }) => {
   return (
     <div className={classes.root}>
       <Paper className={classes.containerPaper}>
-        <Box padding={1}>
-          <Typography variant="h2" className={classes.textContainer}>
-            {farenheight.toFixed(0)}&deg; F
-          </Typography>
-          <Typography>Feels Like: {feelsLike.toFixed(0)}&deg; F</Typography>
-          <Typography>{description}</Typography>
-          <Typography>{city}</Typography>
+        <Box padding={1} className={classes.contentContainer}>
+          <Box className={classes.contentMain}>
+            <Box>
+              <Typography variant="h2" className={classes.textContainer}>
+                {farenheight.toFixed(0)}&deg; F
+              </Typography>
+              <Typography>Feels Like: {feelsLike.toFixed(0)}&deg; F</Typography>
+              <Typography>{description}</Typography>
+            </Box>
+            <SvgIcon className={classes.weatherIcon}>
+              <WeatherIcon className={classes.weatherIcon} />
+            </SvgIcon>
+          </Box>
+          <Typography className={classes.cityName}>{city}</Typography>
         </Box>
       </Paper>
     </div>
